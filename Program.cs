46e06561a99663f52f3dbbdbb0ea9b4b72861a7e
sukhi107 <49531787+sukhi107@users.noreply.github.com>
2022-03-11 09:24:54 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.Write("What is your secret message? ");
+            string message = Console.ReadLine();
+
+            char[] secretMessage = message.ToCharArray();
+
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int index = Array.IndexOf(alphabet, letter);
+                int newIndex = (index + 3) % 26;
+                char newLetter = alphabet[newIndex];
+                encryptedMessage[i] = newLetter;
+            }
+
+            string encrypted = String.Join("", encryptedMessage);
+            Console.WriteLine(encrypted);
+        }
+    }
+}
